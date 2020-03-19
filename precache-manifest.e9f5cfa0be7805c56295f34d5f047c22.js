627ac6c90c3eebc9796bc0a6f51b1c603f47be98
Davis You <davisyou22@gmail.com>
2020-03-18 18:33:46 -0700

    Updates

---
+++ precache-manifest.e9f5cfa0be7805c56295f34d5f047c22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7150621b6865fd6c104267a599eb9a9",
+    "url": "/ai-daw/index.html"
+  },
+  {
+    "revision": "711d27a2296b46e79713",
+    "url": "/ai-daw/static/css/main.cf2bb1bc.chunk.css"
+  },
+  {
+    "revision": "9be8edfc214b84c5e70e",
+    "url": "/ai-daw/static/js/2.f910e375.chunk.js"
+  },
+  {
+    "revision": "3688e54474b5676145820a9125f10fdf",
+    "url": "/ai-daw/static/js/2.f910e375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711d27a2296b46e79713",
+    "url": "/ai-daw/static/js/main.e4d6230a.chunk.js"
+  },
+  {
+    "revision": "e5b1b7e1f2c6934409dd",
+    "url": "/ai-daw/static/js/runtime-main.641cceac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ai-daw/precache-manifest.bcca5d0ada2323d050e086f3a84e4553.js"
+  "/ai-daw/precache-manifest.e9f5cfa0be7805c56295f34d5f047c22.js"
 );
 
 self.addEventListener('message', (event) => {
